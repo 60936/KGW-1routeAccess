49e0aa23fc8938e0a6660cca30acf85e72b3717c
60936 <studio34pl@gmail.com>
2024-02-19 07:07:56 +0800

    r

--- MongoAccess/auth/auth.js
+++ MongoAccess/auth/auth.js
@@ -3,7 +3,7 @@
 // import user model
 const User = require("../User");
 
-// create regiter fn
+// create register fn
 exports.register = async (req, res, next) => {
   const { username, password } = req.body
   if (password.length < 6) {
@@ -24,3 +24,58 @@ exports.register = async (req, res, next) => {
       });
     }
   };
+
+  // update role fn
+  exports.update = async (req, res, next) => {
+    const { role, id } = req.body
+    // Verifying if role and id is present
+    if (role && id) {
+      // Verifying if the value of role is admin
+      if (role === "admin") {
+        await User.findById(id).then((user) => {
+          // Third - Verifies the user is not an admin
+          if (user.role !== "admin") {
+            user.role = role;
+            user.save((err) => {
+              //Monogodb error checker
+              if (err) {
+                res
+                  .status("400")
+                  .json({ message: "An error occurred", error: err.message });
+                process.exit(1);
+              }
+              res.status("201").json({ message: "Update successful", user });
+            });
+          } else {
+            res.status(400).json({ message: "User is already an Admin" });
+          }
+        })
+        .catch((error) => {
+          res
+            .status(400)
+            .json({ message: "An error occurred", error: error.message });
+        });
+      } else {
+        res.status(400).json({
+          message: "Role is not admin",
+        })
+      }
+    } else {
+      res.status(400).json({ message: "Role or Id not present" })
+    }
+  }
+
+  // delete user fn
+  exports.deleteUser = async (req, res, next) => {
+    const { id } = req.body
+    await User.findById(id)
+      .then(user => user.remove())
+      .then(user =>
+        res.status(201).json({ message: "User successfully deleted", user })
+      )
+      .catch(error =>
+        res
+          .status(400)
+          .json({ message: "An error occurred", error: error.message })
+      )
+  }
--- MongoAccess/auth/route.js
+++ MongoAccess/auth/route.js
@@ -3,7 +3,10 @@ const express = require("express")
 const router = express.Router()
 
 // import register fn in routes to be use as route's fn
-const { register } = require("./Auth")
-router.route("/register").post(register)
+const { register, login, update, deleteUser } = require("./Auth")
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/update").put(update);
+router.route("/deleteUser").delete(deleteUser);
 
 module.exports = router
